49eba4521bb28580d6038d19b3b63a08331a36f7
Rob McDougall <rmcdougawww@rogers.com>
2024-10-03 09:21:16 -0400

    ✨ Initial project creation

---
+++ mock-client/src/main/java/com/_4point/aem/watchedfolder/mock_client/WatchedFolderPosterMockClientApplication.java
@@ -0,0 +1,13 @@
+package com._4point.aem.watchedfolder.mock_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchedFolderPosterMockClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchedFolderPosterMockClientApplication.class, args);
+	}
+
+}
---
+++ mock-client/src/test/java/com/_4point/aem/watchedfolder/mock_client/WatchedFolderPosterMockClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com._4point.aem.watchedfolder.mock_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchedFolderPosterMockClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
